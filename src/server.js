bf2f54e5a4918be1f83ebf51f93cfdb548786e29
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-09 11:20:10 +0000

    package json added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import { authenticateDatabase } from './utils/db/connect.js'
-// import pool from './utils/db/connect.js'
-// import productsRouter from './service/products.js'
-// import reviewsRouter from './service/reviews.js'
+// import { authenticateDatabase } from './utils/db/connect.js'
+import pool from './utils/db/connect.js'
+import productsRouter from './service/products.js'
+import reviewsRouter from './service/reviews.js'
 
 const server = express()
 
@@ -10,10 +10,10 @@ const {PORT =5001} = process.env
 // const result = await pool.query("SELECT NOW();")
 
 server.use(express.json())
-// server.use('/products',productsRouter)
-// server.use('/reviews',reviewsRouter)
+server.use('/products',productsRouter)
+server.use('/reviews',reviewsRouter)
 server.listen(PORT, ()=> {
-     authenticateDatabase()
+    //  authenticateDatabase()
     console.log("server has is running ",PORT)
 })
 
