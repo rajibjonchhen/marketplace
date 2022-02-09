5c7be6b9714a874f862e37036506070ae72f9d86
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-09 08:57:00 +0000

    sequelize installed

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from 'express'
-import pool from './utils/db/connect.js'
-import productsRouter from './service/products.js'
-import reviewsRouter from './service/reviews.js'
+import { authenticateDatabase } from './utils/db/connect.js'
+// import pool from './utils/db/connect.js'
+// import productsRouter from './service/products.js'
+// import reviewsRouter from './service/reviews.js'
 
 const server = express()
 
 const {PORT =5001} = process.env
-const result = await pool.query("SELECT NOW();")
+// const result = await pool.query("SELECT NOW();")
 
 server.use(express.json())
-server.use('/products',productsRouter)
-server.use('/reviews',reviewsRouter)
+// server.use('/products',productsRouter)
+// server.use('/reviews',reviewsRouter)
 server.listen(PORT, ()=> {
+     authenticateDatabase()
     console.log("server has is running ",PORT)
 })
 
--- src/utils/db/connect.js
+++ src/utils/db/connect.js
@@ -1,8 +1,25 @@
-import pg from 'pg'
+// import pg from 'pg'
 
 
-const {Pool} = pg
+// const {Pool} = pg
 
-const pool = new Pool()
+// const pool = new Pool()
 
-export default pool
+// export default pool
+
+import Sequelize from "sequelize";
+
+const { POSTGRES_URI, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+  dialect: "postgres",
+});
+
+export const authenticateDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connection has been established successfully.");
+  } catch (error) {
+    console.error("❌ Unable to connect to the database:", error);
+  }
+};
